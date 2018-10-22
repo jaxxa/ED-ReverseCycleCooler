5d1c29617b871de4907221a4475f99335fc1a1bc
Joshua W <jaxxa90@gmail.com>
2018-10-23 07:41:11 +1100

    Updating to 1.0

--- Source/ED-ReverseCycleCooler/Temperature/PlaceWorker_ReverseCycleCooler.cs
+++ Source/ED-ReverseCycleCooler/Temperature/PlaceWorker_ReverseCycleCooler.cs
@@ -9,44 +9,48 @@ namespace EnhancedDevelopment.ReverseCycleCooler
 {
     class PlaceWorker_ReverseCycleCooler : PlaceWorker
     {
-        public override void DrawGhost(ThingDef def, IntVec3 center, Rot4 rot)
+        public override void DrawGhost(ThingDef def, IntVec3 center, Rot4 rot, Color ghostCol)
         {
-            
-            Map _VisibleMap = Find.VisibleMap;
-
-            IntVec3 loc1 = center + IntVec3Utility.RotatedBy(IntVec3.South, rot);
-            IntVec3 loc2 = center + IntVec3Utility.RotatedBy(IntVec3.North, rot);
-            GenDraw.DrawFieldEdges(new List<IntVec3>()
-      {
-        loc1
-      }, Color.magenta);
-            GenDraw.DrawFieldEdges(new List<IntVec3>()
-      {
-        loc2
-      }, Color.white);
-            Room room1 = GridsUtility.GetRoom(loc2, _VisibleMap);
-            Room room2 = GridsUtility.GetRoom(loc1, _VisibleMap);
-            if (room1 == null || room2 == null)
-                return;
-            if (room1 == room2 && !room1.UsesOutdoorTemperature)
+            Map currentMap = Find.CurrentMap;
+            IntVec3 intVec = center + IntVec3.South.RotatedBy(rot);
+            IntVec3 intVec2 = center + IntVec3.North.RotatedBy(rot);
+            List<IntVec3> list = new List<IntVec3>();
+            list.Add(intVec);
+            GenDraw.DrawFieldEdges(list, Color.magenta);
+            list = new List<IntVec3>();
+            list.Add(intVec2);
+            GenDraw.DrawFieldEdges(list, Color.white);
+            RoomGroup roomGroup = intVec2.GetRoomGroup(currentMap);
+            RoomGroup roomGroup2 = intVec.GetRoomGroup(currentMap);
+            if (roomGroup != null && roomGroup2 != null)
             {
-                GenDraw.DrawFieldEdges(Enumerable.ToList<IntVec3>(room1.Cells), new Color(1f, 0.7f, 0.0f, 0.5f));
-            }
-            else
-            {
-                if (!room1.UsesOutdoorTemperature)
-                    GenDraw.DrawFieldEdges(Enumerable.ToList<IntVec3>(room1.Cells), Color.white);
-                if (room2.UsesOutdoorTemperature)
-                    return;
-                GenDraw.DrawFieldEdges(Enumerable.ToList<IntVec3>(room2.Cells), Color.magenta);
+                if (roomGroup == roomGroup2 && !roomGroup.UsesOutdoorTemperature)
+                {
+                    GenDraw.DrawFieldEdges(roomGroup.Cells.ToList(), new Color(1f, 0.7f, 0f, 0.5f));
+                }
+                else
+                {
+                    if (!roomGroup.UsesOutdoorTemperature)
+                    {
+                        GenDraw.DrawFieldEdges(roomGroup.Cells.ToList(), Color.white);
+                    }
+                    if (!roomGroup2.UsesOutdoorTemperature)
+                    {
+                        GenDraw.DrawFieldEdges(roomGroup2.Cells.ToList(), Color.magenta);
+                    }
+                }
             }
         }
 
         public override AcceptanceReport AllowsPlacing(BuildableDef def, IntVec3 center, Rot4 rot, Map map, Thing thingToIgnore = null)
         {
-            if (GenGrid.Impassable(center + IntVec3Utility.RotatedBy(IntVec3.South, rot), map) || GenGrid.Impassable(center + IntVec3Utility.RotatedBy(IntVec3.North, rot), map))
-                return (AcceptanceReport)Translator.Translate("MustPlaceCoolerWithFreeSpaces");
-            return (AcceptanceReport)true;
+            IntVec3 c = center + IntVec3.South.RotatedBy(rot);
+            IntVec3 c2 = center + IntVec3.North.RotatedBy(rot);
+            if (!c.Impassable(map) && !c2.Impassable(map))
+            {
+                return true;
+            }
+            return "MustPlaceCoolerWithFreeSpaces".Translate();
         }
     }
 }
